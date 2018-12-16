1aeded1ec83a92f1fb897ddd61cc6c49837e811f
NuamSkunk1 <45729083+NuamSkunk1@users.noreply.github.com>
2018-12-16 15:46:38 +0330

    Update SignExtend.v

--- verilog-ARM-master/SignExtend.v
+++ verilog-ARM-master/SignExtend.v
@@ -14,16 +14,6 @@ module SignExtend
       2'b00: begin   //unconditional
         sigN = instruction[25];
         out = {{38{sign}}, instruction[25:0]};
-      end
-      
-      2'b10: begin    //conditional 
-        sign = instruction[23];
-        out = {{45{sign}}, instruction[23:5]};
-      end
-      
-      2'b11: begin   // LDUR, STUR
-        sign = instruction[20];
-        out = {{55{sign}}, instruction[20:12]};
       end 
     endcase
   end

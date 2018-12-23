3e3fb0cd85d46623ae9f1ec277f43907b95a2478
NuamSkunk1 <45729083+NuamSkunk1@users.noreply.github.com>
2018-12-23 16:12:46 +0330

    Update SignExtend.v

--- verilog-ARM-master/SignExtend.v
+++ verilog-ARM-master/SignExtend.v
@@ -1,15 +1,12 @@
-module SignExtend32to64
-		#( 
- 		 parameter n = 64,
- 		 parameter delay = 100
-			)
+module SignExtend
+		#(  parameter n = 64,parameter delay = 100)
 			(
 			 input [n-1:0] instruction,
 			 output reg [n-1:0] out);
-	 #delay always @(instruction) begin
+	         #delay always @(instruction) begin
 		case(instruction[31:29])
-		3'b010: out = { {55{instruction[20]}}  , instruction[20:12] }; //D
-		3'b100: out = { {45{instruction[23]}}  , instruction[23:5] }; //CBZ
+			3'b010: out = { {55{instruction[20]}}  , instruction[20:12] }; //D-type
+			3'b100: out = { {45{instruction[23]}}  , instruction[23:5] }; //CBZ-type
 		endcase
 	end
 endmodule

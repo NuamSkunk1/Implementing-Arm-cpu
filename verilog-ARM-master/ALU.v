6b34d96281e9bad1eccd77125dbab6bc416fa481
NuamSkunk1 <45729083+NuamSkunk1@users.noreply.github.com>
2018-12-23 14:24:35 +0330

    Update ALU.v

--- verilog-ARM-master/ALU.v
+++ verilog-ARM-master/ALU.v
@@ -1,30 +1,26 @@
-`timescale 1ns/100ps
+module ALU(
+	input [3:0] operation,
+	input [63:0] i0,
+	input [63:0] i1,
+	output reg [63:0] out,
+	output reg resultZero
+);
 
-module ALU
-  #( 
-    parameter n = 64,
-    parameter delay = 100
-  )
-  
-  (
-  input [n-1 : 0] in1,
-  input [n-1 : 0] in2,
-    input [3:0] operation,
-  output zero,
-    output reg [n-1 : 0] outputs
-  );
-  
-  assign zero = ALUout ? 0 : 1;
-  always @(operation, in1, in2) begin
-    output = 64'bz;
-    case(operation)
-      4'b0000: outputs = in1 & in2;
-      4'b0001: outputs = in1 | in2;
-      4'b0010: outputs = in1 + in2;
-      4'b0110: outputs = in1 - in2;
-      4'b0111: outputs = in2;
-      4'b1100: outputs = in1 ^ in2;
-    endcase
-  end
-    
-  endmodule
+	always
+	begin
+		#10;
+		case(operation)
+			4'b0000: out = i0&i1; // AND
+			4'b0001: out = i0|i1; // OR
+			4'b0010: out = i0+i1; // ADD
+			4'b0110: out = i0-i1; // SUBTRACT
+			4'b0111: out = i1; // PASS INPUT B
+			4'b1100: out = ~(i0|i1); // NOR
+		endcase
+		if(out == 64'b0)
+			resultZero = 1;
+		else
+			resultZero = 0;
+	end
+
+endmodule
